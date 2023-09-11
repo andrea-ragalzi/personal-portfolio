2b7873a6d1cef12daf3e23eefba313e6eb667a7f
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-09-11 23:28:34 +0200

    added translation fot "Are you lost?"

--- personal-portfolio/src/pages/NotFoundPage.tsx
+++ personal-portfolio/src/pages/NotFoundPage.tsx
@@ -1,14 +1,18 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import notFoundImg from "../assets/not_found.png";
+import { useTranslation } from "react-i18next";
 
 const NotFoundPage = () => {
+
+    const { t } = useTranslation();
+
     return (
         <div className="flex flex-col min-h-screen justify-center items-center">
             <div className="flex flex-col w-full xl:w-3/4">
                 <Navbar />
                 <div className="md:w-1/2 mx-auto xl:mt-20 text-center border-b-2 border-palette-400 dark:border-palette-800 pb-10">
-                    <h2>Page not found</h2>
+                    <h2>{t('Are you lost?')}</h2>
                     <img src={notFoundImg} alt="" />
                 </div>
             </div>
