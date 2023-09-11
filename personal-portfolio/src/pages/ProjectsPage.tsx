9e488da6216a57cdc944a3617462e0ef13374eac
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-09-11 23:48:42 +0200

    refactoring

--- personal-portfolio/src/pages/ProjectsPage.tsx
+++ personal-portfolio/src/pages/ProjectsPage.tsx
@@ -7,11 +7,6 @@ import { useState } from "react";
 const ProjectsPage = () => {
     const [activeIndex, setActiveIndex] = useState(0);
 
-    const handleMouseEnter = (index: number) => {
-        setActiveIndex(index);
-        console.log("Enter");
-    };
-
     return (
         <div className="flex flex-col min-h-screen justify-center items-center">
             <div className="flex flex-col w-full xl:w-3/4">
@@ -20,7 +15,7 @@ const ProjectsPage = () => {
                     <div
                         key={index}
                         className={`border-b-2 border-palette-400 dark:border-palette-800 mb-10 pb-5`}
-                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseEnter={() => setActiveIndex(index)}
                     >
                         <ProjectDetail
                             title={project.title}
